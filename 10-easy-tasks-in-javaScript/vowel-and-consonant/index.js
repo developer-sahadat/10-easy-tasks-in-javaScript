2f13ca537c8ae89452cb1ff4c0490f9706e9e662
developer-sahadat <mdridoysikder862@gmail.com>
2022-01-31 16:46:16 +0600

    vowel and consonant

---
+++ 10-easy-tasks-in-javaScript/vowel-and-consonant/index.js
@@ -0,0 +1,25 @@
+
+//vowel and consonant
+
+let letter=prompt("Enter Your letter ?");
+letter=letter.toLowerCase();
+
+if(letter=="a"){
+    console.log("vowel");
+}
+else if(letter=="e"){
+    console.log("Vowel");
+}
+else if(letter=="i"){
+    console.log("Vowel");
+
+}
+
+else if(letter=="i"){
+    console.log("Vowel");
+}
+else if(letter=="u"){
+console.log("vowel");
+}else{
+    console.log("Consonant");
+}
