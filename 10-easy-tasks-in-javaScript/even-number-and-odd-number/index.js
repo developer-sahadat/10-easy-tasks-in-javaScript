9077f7ecb612e9597bde6a1adfee89f7d5eaaf08
developer-sahadat <mdridoysikder862@gmail.com>
2022-01-31 17:09:46 +0600

    Even number and odd number

---
+++ 10-easy-tasks-in-javaScript/even-number-and-odd-number/index.js
@@ -0,0 +1,26 @@
+//Even number and odd number
+
+/*
+let x=1;
+do{
+
+    console.log(x);
+
+    x++;
+
+}while(x<=100)
+*/
+
+// for(let x=0; x<=100; x++){
+//     if(x %2==0){
+//         console.log(x);
+//     }
+// }
+
+let i=50;
+while(i<=80){
+    if(i %2!=0){
+        console.log(i);
+    }
+    i++;
+}
